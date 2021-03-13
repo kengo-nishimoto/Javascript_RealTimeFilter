63c8240c6fde4b84659b4d28aa4fefc14270c900
EC2 Default User <ec2-user@ip-172-31-40-246.us-east-2.compute.internal>
2021-03-13 08:23:32 +0000

    filter

---
+++ main.js
@@ -0,0 +1,19 @@
+function Search(text){
+    $(".keyword-list").each(function(index, element){
+        let keyword = $(element).text();
+        if(keyword.indexOf(text) === -1){
+            $(element).parent().css("display", "none");
+        }else{
+            $(element).parent().css("display", "block");
+        }
+    });
+}
+
+$(".input-text").on("input", function(event){
+    let text = $(".input-text").val();
+    if(text === ""){
+        $(".filter-img").css("display", "block");
+    }else{
+        Search(text);
+    }
+});
